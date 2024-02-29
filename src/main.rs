004d20e8e110b38997ad90a5e94a868adc33c81b
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-29 14:42:32 -0500

    update to search

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,18 @@ fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         link { rel: "stylesheet", href: "../dist/output.css" },
         div {
-            class: "w-full h-screen bg-gray-300 flex items-center justify-center",
-            "Hello, world!"
+            class: "w-full h-screen flex items-center justify-center",
+            div {
+                div {
+                    class: "relative mb-4 flex w-full flex-wrap items-stretch",
+                    input {
+                        class: "relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary",
+                        placeholder: "Enter a word",
+                        autofocus: "true",
+                    }
+                }
+            }
+            
         }
     })
 }
