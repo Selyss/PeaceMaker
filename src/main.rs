193b5c4bb935c446ebcc276f31c4e6d78730ed7f
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-29 21:00:41 -0500

    no idea whats happening

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 use dioxus::prelude::*;
 use dioxus_desktop::{Config, WindowBuilder};
 use dioxus_material_icons::{MaterialIcon, MaterialIconColor};
+use rand::prelude::*;
+
+#[derive(Props)]
+struct LetterProps<'a> {
+    letter: &'a str,
+}
 
 fn main() {
     // launch the dioxus app in a webview
@@ -52,6 +58,28 @@ fn word_input<'a>(cx: Scope<'a>) -> Element<'a> {
                     }
                 }
             }
+        }
+    })
+}
+
+fn add_tile<'a>(cx: Scope<'a, ch: LetterProps<'a>>) -> Element<'a> {
+    let mut rng = rand::thread_rng();
+    let rotate_value: f64 = rng.gen_range(-1.5..=1.5);
+    let rotate_string = format!("--rotate: {};", rotate_value).as_str();
+    cx.render(rsx! {
+        div {
+            class: "tile",
+            "data-letter": ch,
+            style: rotate_string,
+            ch
+        }
+    })
+}
+
+fn remove_last_tile<'a>(cx: Scope<'a>, ch: &str) -> Element<'a> {
+    cx.render(rsx! {
+        div {
+            "a"
         }
     })
 }
