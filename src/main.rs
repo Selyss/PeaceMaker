43bb7e0a49c35c9d928224d9be10033099c74f84
Selyss <99344963+Selyss@users.noreply.github.com>
2024-03-02 20:01:37 -0500

    whole lotta hacks

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,55 @@ fn main() {
 // define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        link { rel: "stylesheet", href: "../dist/output.css" },
-        // div {
-        //     class: "w-full h-screen flex items-center justify-center",
-        //     div {
-        //         div {
-        //             class: "relative mb-4 flex w-full flex-wrap items-stretch",
-                    word_input {}
-        //         }
-        //     }
+        link { rel: "stylesheet", href: "../dist/output.css" }
+        div {
+            class: "bg-[#a08cb4]",
+            div {
+                class: "container m-auto grid h-screen w-screen grid-cols-6 content-center gap-4",
+                empty_space(cx)
+                empty_space(cx)
+                empty_space(cx)
+                empty_space(cx)
+                empty_space(cx)
+                empty_space(cx)
+                    tile(cx)
+                    tile(cx)
+                    tile(cx)
+                    tile(cx)
+                    tile(cx)
+                    tile(cx)
+                }
 
-        // }
+
+
+        }
+
     })
 }
 
+fn empty_space<'a>(cx: Scope<'a>) -> Element<'a> {
+    cx.render(rsx! {
+                div {
+                    class: "shi h-24 w-24 rounded-lg bg-[#504464]",
+                }
+
+    })
+}
+fn tile<'a>(cx: Scope<'a>) -> Element<'a> {
+    cx.render(rsx! {
+                    div {
+                        class: "sho size-28 h-24 w-24 rounded-lg border-2 border-[#a78f5e] bg-wood",
+                        p {
+                            class: "mt-3 text-center align-middle text-6xl font-bold",
+                            "A"
+                        }
+
+                    }
+
+    })
+}
+
+#[warn(dead_code)]
 fn word_input<'a>(cx: Scope<'a>) -> Element<'a> {
     let input = use_state(cx, String::new);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,rs}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        wood: "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/Melamine-wood-001.png')",
+      },
+    },
   },
   plugins: [],
 };
